009e57af25efe2d5906bee0736fe63a4d7e1668f
luisabwk <luisa.barwinski@gmail.com>
2025-04-09 14:15:43 -0300

    Update urlParam.js

--- urlParam.js
+++ urlParam.js
@@ -51,7 +51,7 @@
 
   // Função para montar a URL de redirecionamento com os parâmetros obtidos dos cookies
   function urlBuilder() {
-    const baseUrl = 'https://www.trinks.com/programa-para-salao-de-beleza/cadastrar-meu-estabelecimento';
+    const baseUrl = 'Sua URL aqui'; // Cole sua URL aqui
     const params = new URLSearchParams({
       referrer: getCookie('referrer') || '',
       landingUrl: getCookie('landingUrl') || '',
